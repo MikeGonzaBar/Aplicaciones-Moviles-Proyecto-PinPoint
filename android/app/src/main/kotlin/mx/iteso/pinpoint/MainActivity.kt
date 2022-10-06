ba256c9c66de42076b66f0b62014d70c4f3fe81c
Miguel González Barajas <is722298@iteso.mx>
2022-10-06 11:46:05 -0500

    feat: initial commit, created main flutter project

---
+++ android/app/src/main/kotlin/mx/iteso/pinpoint/MainActivity.kt
@@ -0,0 +1,6 @@
+package mx.iteso.pinpoint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pinpoint/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+    );
+  }
+}
