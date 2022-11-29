93b90a6eb34a6233b6ce979c73efe1448573d68c
Miguel González Barajas <is722298@iteso.mx>
2022-11-29 00:48:14 -0600

    feat: deleted unused import

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,8 +3,6 @@ import 'package:flutterfire_ui/auth.dart';
 import 'package:pinpoint/pages/main_page.dart';
 import 'package:pinpoint/pages/register.dart';
 
-import '../keys/keys.dart';
-
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
